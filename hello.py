cd8836115a79e40ae511abbae188f1b96d6c0e61
charlespaul1 <charlespaulwabomba@gmail.com>
2023-05-11 08:23:06 +0300

    string manipulation

---
+++ hello.py
@@ -0,0 +1,20 @@
+print("Hello America")
+x = 5
+print(x)
+# multi line string
+
+word = "this is a multi line string"\
+    "so this is a new line"\
+        "and this is another line"
+print(word)
+# concatenation
+name = "Je"
+nameTwo = "Mappelle"
+
+print(name +" "+ nameTwo)
+# accessing string characters
+name = "marie"
+# last character being accessed
+print(name[-1])
+# finding the length of a string
+print(len(name))
